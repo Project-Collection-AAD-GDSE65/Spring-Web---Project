323c0f2f8168c8d9d9d420b738ae9856147818cf
ThanuraSilva <thanurasilva1989@gmail.com>
2024-03-09 10:54:39 +0530

    Custom exception created

---
+++ src/main/java/lk/ijse/gdse/aad65/student_managemant_system/exception/InvalidDataException.java
@@ -0,0 +1,7 @@
+package lk.ijse.gdse.aad65.student_managemant_system.exception;
+
+public class InvalidDataException extends RuntimeException{
+    public InvalidDataException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/lk/ijse/gdse/aad65/student_managemant_system/exception/NotFoundException.java
@@ -0,0 +1,7 @@
+package lk.ijse.gdse.aad65.student_managemant_system.exception;
+
+public class NotFoundException extends RuntimeException {
+    public NotFoundException(String message) {
+        super(message);
+    }
+}
